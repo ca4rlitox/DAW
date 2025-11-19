e4872f8d2be7614e8cd73a06376f4288fea9eb86
ca4rlitox <ch4rl1e.m.c@pm.me>
2025-11-19 19:19:15 +0100

    tres en raya casi listo

--- Programación/ProgramacionJava/untitled/src/ud4/tresEnRaya.java
+++ Programación/ProgramacionJava/untitled/src/ud4/tresEnRaya.java
@@ -10,6 +10,8 @@ public class tresEnRaya {
         int opcion;
         int jugador1;
         int jugador2;
+        int estadisticas=0;
+        int estadisticas2=0;
 
         do {
             System.out.println("1. Jugar una partida");
@@ -21,23 +23,46 @@ public class tresEnRaya {
             switch (opcion) {
 
                 case 1:
-                    dibujaTablero();
                     iniciar();
-                    System.out.println("Introduzca movimiento (Jugador 1):\n");
-                    jugador1 = in.nextInt();
-                    mueveJugador1(jugador1);
-                    dibujaTablero();
+                    do {
 
+                        dibujaTablero();
+                        System.out.println("Introduzca movimiento (Jugador 1):\n");
+                        jugador1 = in.nextInt();
+                        mueveJugador1(jugador1 - 1);
+                        dibujaTablero();
+
+                        if (ganaJugador1()) {
+                            System.out.println("Ha ganado el jugador 1!\n");
+                            estadisticas++;
+                            break;
+                        }
+
+                        System.out.println("Introduzca movimiento (Jugador 2):\n");
+                        jugador2 = in.nextInt();
+                        mueveJugador2(jugador2 - 1);
+                        if (ganaJugador2()) {
+                            dibujaTablero();
+                            System.out.println("Ha ganado el jugador 2!\n");
+                            estadisticas2++;
+                            break;
+                        }
+
+
+                    } while (quedanCasillas());
 
                     break;
 
                 case 2:
-
+                    System.out.println("\n Estadisticas:\nEl jugador 1 lleva "+estadisticas+" partidas ganadas, y el jugador 2 lleva "+estadisticas2+" partidas ganadas\n");
                     break;
 
+                case 3:
+                    System.out.println("Has salido.");
+                    break;
 
                 default:
-                    System.out.println("Opción no válida. Inténtalo otra vez.");
+                    System.out.println("Opción no válida. Inténtalo otra vez.\n");
 
             }
 
@@ -49,20 +74,7 @@ public class tresEnRaya {
     public static void dibujaTablero() {
 
         System.out.println("-------------\n" +
-                "| 1 | 2 | 3 |\n" +
-                "-------------\n" +
-                "| 4 | 5 | 6 |\n" +
-                "-------------\n" +
-                "| 7 | 8 | 9 |\n" +
-                "-------------");
-
-    }
-
-
-    public static void iniciar() {
-
-        System.out.print("-------------\n" +
-                "| "+tablero[0]+" | "+tablero [1]+" | "+tablero [2]+" |\n" +
+                "| "+tablero[0]+" | "+tablero[1]+" | "+tablero[2]+" |\n" +
                 "-------------\n" +
                 "| "+tablero[3]+" | "+tablero[4]+" | "+tablero[5]+" |\n" +
                 "-------------\n" +
@@ -71,59 +83,134 @@ public class tresEnRaya {
 
     }
 
+
+    public static void iniciar() {
+
+        char vaciar=' ';
+        for (int i=0;i<9;i++) {
+            tablero[i] = vaciar;
+        }
+
+    }
+
     public static void mueveJugador1(int pos) {
 
-        if (pos==0) {
-            tablero[0] = 'x';
-        }
-        if (pos==1) {
-            tablero[1] = 'x';
-        }
-        if (pos==2) {
-            tablero[2] = 'x';
-        }
-        if (pos==3) {
-            tablero[3] = 'x';
-        }
-        if (pos==4) {
-            tablero[4] = 'x';
-        }
-        if (pos==5) {
-            tablero[5] = 'x';
-        }
-        if (pos==6) {
-            tablero[6] = 'x';
-        }
-        if (pos==7) {
-            tablero[7] = 'x';
-        }
-        if (pos==8) {
-            tablero[8] = 'x';
-        }
+        movimientoValido(pos);
+        tablero[pos] = 'X';
 
     }
 
 
     public static void mueveJugador2(int pos) {
 
+        movimientoValido(pos);
+        tablero[pos] = 'O';
 
     }
 
     public static boolean movimientoValido(int pos) {
+        int validacion;
+        if ((tablero[pos]=='X') || (tablero[pos]=='O')) {
+            do {
 
-        return true;
+                System.out.println("Movimiento no válido. Inténtalo otra vez");
+                pos = in.nextInt();
+                pos = pos - 1;
+                validacion = pos;
 
+            } while ((tablero[pos] == 'X') || (tablero[pos] == 'O'));
+
+        }
+
+        return false;
     }
 
-    public static boolean ganaJugador1() {
+    public static boolean quedanCasillas() {
 
-        return true;
+        for (int i = 0;i<9;i++) {
+            if (tablero[i]==' ') {
+                return true;
+            }
+        }
+                return false;
+
+        }
+
+
+    public static boolean ganaJugador1() {
+        if ((tablero[0]=='X') && (tablero[4]=='X') && (tablero[8]=='X')) {
+
+            return true;
+        }
+        else if ((tablero[2]=='X') && (tablero[4]=='X') && (tablero[6]=='X')) {
+
+            return true;
+        }
+        else if ((tablero[0]=='X') && (tablero[1]=='X') && (tablero[2]=='X')) {
+
+            return true;
+        }
+        else if ((tablero[3]=='X') && (tablero[4]=='X') && (tablero[5]=='X')) {
+
+            return true;
+        }
+        else if ((tablero[6]=='X') && (tablero[7]=='X') && (tablero[8]=='X')) {
+
+            return true;
+        }
+        else if ((tablero[0]=='X') && (tablero[3]=='X') && (tablero[6]=='X')) {
+
+            return true;
+        }
+        else if ((tablero[1]=='X') && (tablero[4]=='X') && (tablero[7]=='X')) {
+
+            return true;
+        }
+        else if ((tablero[2]=='X') && (tablero[5]=='X') && (tablero[8]=='X')) {
+
+            return true;
+        }
+
+        return false;
 
     }
 
     public static boolean ganaJugador2() {
 
-return true;
+        if ((tablero[0]=='O') && (tablero[4]=='O') && (tablero[8]=='O')) {
+
+            return true;
+        }
+        else if ((tablero[2]=='O') && (tablero[4]=='O') && (tablero[6]=='O')) {
+
+            return true;
+        }
+        else if ((tablero[0]=='O') && (tablero[1]=='O') && (tablero[2]=='O')) {
+
+            return true;
+        }
+        else if ((tablero[3]=='O') && (tablero[4]=='O') && (tablero[5]=='O')) {
+
+            return true;
+        }
+        else if ((tablero[6]=='O') && (tablero[7]=='O') && (tablero[8]=='O')) {
+
+            return true;
+        }
+        else if ((tablero[0]=='O') && (tablero[3]=='O') && (tablero[6]=='O')) {
+
+            return true;
+        }
+        else if ((tablero[1]=='O') && (tablero[4]=='O') && (tablero[7]=='O')) {
+
+            return true;
+        }
+        else if ((tablero[2]=='O') && (tablero[5]=='O') && (tablero[8]=='O')) {
+
+            return true;
+        }
+
+        return false;
 
     }
 
