a8ee02442fc94062687647dea386e9b6c122fffb
Carlos Martin Codina <ch4rl1e.m.c@pm.me>
2026-01-20 12:31:02 +0100

    Ejercicio 2 personas

--- Programación/ProgramacionJava/untitled/src/ud6/Persona.java
+++ Programación/ProgramacionJava/untitled/src/ud6/Persona.java
@@ -6,21 +6,42 @@ public class Persona {
     private int edad;
     private double altura,peso;
 
-    public Persona (String nombre, String dni, int edad, double altura, double peso, char sexo) {
-        this.nombre = nombre;
-        this.dni = dni;
-        this.edad = edad;
-        this.altura = altura;
-        this.peso = peso;
+    public Persona() {
+        nombre="";
+        sexo='H';
+        edad=0;
+        dni="";
+        altura=0;
+        peso=0;
     }
+
     public Persona (String nombre, int edad,char sexo) {
         this.nombre = nombre;
-        this.edad = edad;
         this.sexo = sexo;
-        edad=0;
+        this.edad = edad;
         dni="";
         altura=0;
         peso=0;
 
     }
+    public Persona (String nombre, String dni, int edad, double altura, double peso, char sexo) {
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.dni = dni;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String setNombre(String nombre) {
+        return nombre = nombre;
+    }
+    public String setSexo(char sexo) {
+        if  (sexo=='M') return "M";
+        if  (sexo=='m') return "M";
+        if  (sexo=='F') return "F";
+        if  (sexo=='f') return "F";
+        return null;
+    }
+
 }
