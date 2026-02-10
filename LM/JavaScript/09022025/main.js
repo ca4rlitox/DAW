8e7543a7bbb1a95d084d0eca7b16558c69df1fc7
Carlos Martin Codina <ch4rl1e.m.c@pm.me>
2026-02-10 10:18:00 +0100

    Hola

---
+++ LM/JavaScript/09022025/main.js
@@ -0,0 +1,10 @@
+switch (value) {
+    case 'a':
+        console.log('Value is a');
+        break;
+    case 'b':
+        console.log('Value is b');
+        break;
+    default:
+        console.log('Value is something else');
+}
