aae65920b4340e36f8b11990d07e2238e98e4975
Carlos Martin Codina <ch4rl1e.m.c@pm.me>
2026-02-16 14:08:21 +0100

    Hoja 2 ej 3.

---
+++ Programación/ProgramacionJava/untitled/src/ud7/UsoUniversidad.java
@@ -0,0 +1,8 @@
+package ud7;
+
+public class UsoUniversidad {
+    public static void main(String[] args) {
+        Profesor profe1 = new Profesor("Pablo","moreno","54534534H","casado","04/12/2024",4,"mates");
+        System.out.println(profe1);
+    }
+}
