5146836012a04b0fd54dffee20ff98e112ee36a7
Carlos Martin Codina <ch4rl1e.m.c@pm.me>
2026-03-05 14:19:28 +0100

    python poo

--- Python/1DAW/POO/poo1.py
+++ Python/1DAW/POO/poo1.py
@@ -1,8 +1,31 @@
 
 # Las clases se definen con la primera en mayúscula y en singular
 class Alumno:
-    def __init__(self, edad):
-        self.edad = edad
+    # Constructor
+    #self es como el this de java
+    def __init__(self, nombre, edad, marica):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__marica = marica
 
-trista=Alumno(25)
-carlos=Alumno(22)
+    ## Para ver datos
+    def verDatos(self):
+        print(f"Nombre: {self.__nombre} edad: {self.__edad} marica: {self.__marica}") # el ultimo atributo es uno protegido por el __
+    ## Para ver datos o establecerlos (metodo java)
+    # def setEdad(self,ed): #Como en Java el setter
+    #    self.__edad = ed
+    # def getEdad(self):
+    #    return self.__edad
+    #metodo de python
+    @property
+    def edad(self):
+        return self.__edad
+
+trista=Alumno("Leandro Trista", 33, "no")
+carlos=Alumno("Carlos Martin",21,"si")
+
+
+#public: edad
+#protected: _edad (dentro de la clase)
+#private: __private (dentro de la clase)
+print(carlos.edad)
