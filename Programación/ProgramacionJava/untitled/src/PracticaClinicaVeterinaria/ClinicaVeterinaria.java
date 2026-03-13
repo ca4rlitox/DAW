823121542186a399d290fba62719519f41efafc0
Carlos Martin Codina <ch4rl1e.m.c@pm.me>
2026-03-13 12:57:20 +0100

    clinica veterinaria casi hecho

--- Programación/ProgramacionJava/untitled/src/PracticaClinicaVeterinaria/ClinicaVeterinaria.java
+++ Programación/ProgramacionJava/untitled/src/PracticaClinicaVeterinaria/ClinicaVeterinaria.java
@@ -6,7 +6,7 @@ public class ClinicaVeterinaria {
     private ArrayList<Animal> animales;
 
     public ClinicaVeterinaria(){
-        ArrayList<Animal> animals = new ArrayList<>();
+        animales = new ArrayList<>();
     }
 
     public void insertaAnimal(Animal animal) {
@@ -14,11 +14,32 @@ public class ClinicaVeterinaria {
     }
     public String buscaAnimal(String nombre) {
 
-        for (Animal animal: animales) {
-            if (animales.contains(animal)) {
-                return animal.dameDatosAnimal();
+        for (int i = 0; i < animales.size(); i++) {
+            if (animales.get(i).getNombre().equals(nombre)) {
+            return animales.get(i).dameDatosAnimal();
             }
         }
         return null;
+    }
+    public boolean modificaComentarioAnimal(String animal, String comentario) {
+        if (animales.isEmpty()) return false;
+        Animal a=animales.get(0);
+        for (int i = 0; i < animales.size(); i++) {
+            if (animales.get(i).getNombre().equals(animal)) {
+                a = animales.get(i);
+            }
+        }
+        if (a == null) {
+            return false;
+        }
+        a.setComentarios(comentario);
+        return true;
+    }
+    public String toString() {
+        String cad="";
+        for(int i = 0; i < animales.size(); i++){
+            cad+=animales.get(i).toString()+"\n";
+        }
+        return cad;
     }
 }
--- Programación/ProgramacionJava/untitled/src/PracticaClinicaVeterinaria/UsoClinicaVeterinaria.java
+++ Programación/ProgramacionJava/untitled/src/PracticaClinicaVeterinaria/UsoClinicaVeterinaria.java
@@ -2,11 +2,12 @@ package PracticaClinicaVeterinaria;
 
 public class UsoClinicaVeterinaria {
     public static void main(String[] args) {
-        Gato gato1 = new Gato ("Firulais", "01/04/2005",25,"Persa","hy45tbc7842tg");
         ClinicaVeterinaria cv = new ClinicaVeterinaria();
 
-        cv.insertaAnimal(gato1);
+        cv.insertaAnimal(new Gato("Firulais","01/04/2004",1,"Persa","fdsjfndsjuf"));
+        cv.insertaAnimal(new Perro("Firulais","01/04/2004",1,"Persa","fdsjfndsjuf"));
 
-        System.out.println(cv.buscaAnimal("Firulais"));
+        System.out.println(cv);
+
     }
 }
