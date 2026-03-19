ff0289833a1ddbdad00a5d58a2cc7a5075c30a4a
Carlos Martin Codina <ch4rl1e.m.c@pm.me>
2026-03-19 14:18:55 +0100

    diccionarios

--- Python/1DAW/POO/ej4Diccionario.py
+++ Python/1DAW/POO/ej4Diccionario.py
@@ -1,13 +1,34 @@
-class listaTareas:
-    def __init__(self,identificador,titulo,prioridad,estaRealizada):
-        self.diccionario = {}
-        self.identificador = identificador
-        self.titulo = titulo
-        self.prioridad = prioridad
-        self.estaRealizada = estaRealizada
-        self.diccionario+= {self.identificador, self.titulo, self.prioridad, True}
+class Tarea:
+    __listaTareas = {}
+    def __init__(self,identificador,titulo,prioridad):
+        self.__id = identificador
+        self.__titulo = titulo
+        self.__prioridad = prioridad
+        self.__completada = False
+        if identificador in Tarea.__listaTareas:
+            print("La tarea ya existe")
+        else:
+            Tarea.__listaTareas[identificador] = self
 
 
+    @classmethod
+    def mostrarTareasNoCompletadas(cls):
+        print("Lista de tareas:")
+        for tarea in cls.__listaTareas:
+            print(f"[{tarea.__id}]")
 
-hola = listaTareas("P43","Marica",9,True)
+    @classmethod
+    def marcarComoCompletada(cls,id):
+        if id in Tarea.__listaTareas:
+            tarea = cls.__listaTareas[id]
+            tarea.__completada = True
+            print("Tarea marcada como completada.")
+        else:
+            print("La tarea",id,"no existe.")
 
+t1 = Tarea("P43","Comprar pollo", 9)
+t3 = Tarea("p47","Comprar pollo", 9)
+t2 = Tarea("P43","Duolingo", 4)
+
+print(t1.mostrarTareasNoCompletadas)
+t1.marcarComoCompletada(t1)
